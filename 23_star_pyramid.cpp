39f16a279513c408d0e936c37287c7e7f1aae16c
krishnadhar pandey <krishnadhargzp00@gmail.com>
2024-08-02 14:10:22 +0530

    twenty three

---
+++ 23_star_pyramid.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)  // For space printing.
+        {
+            cout<<" "<<" ";
+        }
+        for(int j=1;j<=i;j++)  // For star printing.
+        {
+            cout<<"*"<<" ";
+        }
+        for(int j=1;j<=i-1;j++)  // For star printing.
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
