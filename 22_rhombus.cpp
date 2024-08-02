dcdfbac35345b647927dc246b785d1c3d40cab55
krishnadhar pandey <krishnadhargzp00@gmail.com>
2024-08-02 14:09:39 +0530

    twenty two

---
+++ 22_rhombus.cpp
@@ -0,0 +1,21 @@
+// Important Question- Medium level question.
+// Inside one loop there is more than one loop. i.e two loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)  // For space printing.
+        {
+            cout<<" "<<" ";
+        }
+        for(int j=1;j<=n;j++)  // For star printing.
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
