476895ad4ed32a4eb9c0943fd3410a9a6845f6f5
krishnadhar pandey <krishnadhargzp00@gmail.com>
2024-08-02 14:08:27 +0530

    twentyone commit

---
+++ 21_number_traingle_reverse.cpp
@@ -0,0 +1,22 @@
+// number traingle vertically flipped.
+// Important Question- Medium level question.
+// Inside one loop there is more than one loop. i.e two loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)  // For space printing.
+        {
+            cout<<" "<<" ";
+        }
+        for(int j=1;j<=i;j++)  // For star printing.
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
